64e08ddfc850b5d66a92b4693cdd846192bfa049
Wuizlz <brisenodan12@gmail.com>
2026-02-25 13:20:44 -0600

    fixed dependicies

---
+++ jest.transform.cjs
@@ -0,0 +1,5 @@
+const tsJest = require('ts-jest');
+
+// Jest expects the transformer module itself to expose createTransformer/process.
+// ts-jest exposes it on the default export in this version.
+module.exports = tsJest.default ?? tsJest;
